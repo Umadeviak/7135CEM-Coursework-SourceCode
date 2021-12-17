04649f5a8abdfdaafff07f449227c4da92150c1f
Umadeviak <84017198+Umadeviak@users.noreply.github.com>
2021-12-17 15:58:07 +0000

    Add files via upload

---
+++ GA.m
@@ -0,0 +1,19 @@
+
+clc, clear, close all
+opt = optimoptions('ga','PlotFcn', {@gaplotbestf, @gaplotdistance});
+for i = 1:15
+    initial_flag = 0;
+    [x,fval,exit_flag,opt] = ga(@(x) benchmark_func(x,10),2,opt);
+    %[x,fval,exit_flag,opt] = ga(@(x) benchmark_func(x,10),10,opt);
+    %[x,fval,exit_flag,opt] = ga(@(x) benchmark_func(x,11),2,opt);
+    %[x,fval,exit_flag,opt] = ga(@(x) benchmark_func(x,11),10,opt);
+    val(i) = fval;
+    option(i) = opt;
+end
+max = max(val);
+min = min(val);
+mean = mean(val);
+std = std(val);
+values = val;
+
+
